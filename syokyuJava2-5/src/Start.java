7526f78ee4b192b22a6d2c1d252dd85199fba352
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-14 12:18:53 +0900

    初級Java3.4のコミット

--- syokyuJava2-5/src/Start.java
+++ syokyuJava2-5/src/Start.java
@@ -4,8 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		
 		System.out.println("プログラムを実行します。");
-		MyFrame frame1=new RectTest();
-
+		//MyFrame frame1=new RectTest();
+		MyFrame frame1=new OvalTest();
 	}
 
 }
