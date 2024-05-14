0d6b9f13742904fb1280f9d279ca1c6c13d3bcbb
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-14 11:49:31 +0900

    3.2四色形追加

--- syokyuJava2-5/src/RectTest.java
+++ syokyuJava2-5/src/RectTest.java
@@ -2,7 +2,16 @@
 public class RectTest extends MyFrame {
 	
 	public void run() {
-		fillRect(30,50,200,100);
+		//fillRect(s,t ,w,h);
+		fillRect(30,40,50,100);
+		
+		fillRect(130,40,50,100);
+		
+		fillRect(90,160,30,30);
+		
+		fillRect(30,200,150,10);
+		
+		
 	}
 
 }
