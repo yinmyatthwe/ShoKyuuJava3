351bdca2c3868be3084920864ddc231aadede1a4
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-14 12:11:13 +0900

    初級Java3.3のコミット

--- syokyuJava2-5/src/RectTest.java
+++ syokyuJava2-5/src/RectTest.java
@@ -3,13 +3,22 @@ public class RectTest extends MyFrame {
 	
 	public void run() {
 		//fillRect(s,t ,w,h);
+		
+		//黄
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
-		
+		//紫
+		setColor(255,0,255);
 		fillRect(130,40,50,100);
-		
-		fillRect(90,160,30,30);
-		
-		fillRect(30,200,150,10);
+        //水色
+		setColor(176,200,255);
+		fillRect(230,40,50,100);
+		//黒
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+		//gray
+		setColor(128,128,120);
+		fillRect(30,200,150,30);
 		
 		
 	}
