b8cf6d8f0bce5a39ff9895b302da3bc7f831596f
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-20 11:07:26 +0900

    while文を利用する

---
+++ syokyuJava2-5/src/RectLoop.java
@@ -0,0 +1,16 @@
+
+public class RectLoop extends MyFrame {
+	public void run() {
+
+		setColor(0,128,0);
+		int x=30;
+		while(x<=170) {
+			System.out.println(x);
+			fillRect(x,80,10,100);
+			x+=20;
+			
+		}
+		
+	}
+
+}
--- syokyuJava2-5/src/Start.java
+++ syokyuJava2-5/src/Start.java
@@ -6,7 +6,7 @@ public class Start {
 		System.out.println("プログラムを実行します。");
 		//MyFrame frame1=new RectTest();
 		//MyFrame frame1=new OvalTest();
-		MyFrame frame1=new ManyFigures();
+		MyFrame frame1=new RectLoop();
 	}
 
 }
