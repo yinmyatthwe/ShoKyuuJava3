6f99af53c4102c9e1b825584279bb8843e86da63
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-20 10:56:30 +0900

    4.1図形をたくさん描く

---
+++ syokyuJava2-5/src/ManyFigures.java
@@ -0,0 +1,11 @@
+
+public class ManyFigures extends MyFrame{
+	public void run() {
+		setColor(0,128,0);
+		fillRect(30,80,10,100);
+		fillRect(50,80,10,100);
+		fillRect(70,80,10,100);
+		fillRect(90,80,10,100);
+	}
+
+}
