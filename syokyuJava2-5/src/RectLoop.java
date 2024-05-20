8b8e4a443e0b1df47b5366b88e8b2c18cb886a2c
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-20 11:54:09 +0900

    確認問題4.3

--- syokyuJava2-5/src/RectLoop.java
+++ syokyuJava2-5/src/RectLoop.java
@@ -3,8 +3,8 @@ public class RectLoop extends MyFrame {
 	public void run() {
 
 		setColor(0,128,0);
-		int x=0;
-		for(int i=1;i<15;i++) {
+		int x=30;
+		for(int i=1;i<10;i++) {
 			System.out.println(x);
 			fillRect(x,80,10,100);
 			x+=20;
