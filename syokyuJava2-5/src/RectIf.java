8ee72be9ebe5e2c440928be51c0054f2a0bef4da
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-20 12:12:07 +0900

    確認問題4.4

--- syokyuJava2-5/src/RectIf.java
+++ syokyuJava2-5/src/RectIf.java
@@ -6,8 +6,10 @@ public class RectIf extends MyFrame{
 		setColor(0,128,0);
 		int i;
 		for(i=0;i<9;i++) {
-			if(i>=4 && i<7){
-				fillRect(x,100,10,100);
+			if(i==0){
+				fillRect(x,60,10,100);
+			}else if(i==8) {
+				fillRect(x, 100, 10, 100);
 			}
 			else {
 				fillRect(x,80,10,100);
