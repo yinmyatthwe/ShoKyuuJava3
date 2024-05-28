b64821d4de6fde5fe036410a86f8ebc54159f423
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-05-28 10:11:36 +0900

    確認問題6.2

--- syokyuJava2-5/src/DisplayFaceObject.java
+++ syokyuJava2-5/src/DisplayFaceObject.java
@@ -1,11 +1,14 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1=new Face(50,50);
+		Face face1=new Face(30,50);
 		face1.draw(this);
 		
-		Face face2=new Face(200,100);
+		Face face2=new Face(230,50);
 		face2.draw(this);
+		
+		Face face3=new Face(130,150);
+		face3.draw(this);
 	}
 
 }
