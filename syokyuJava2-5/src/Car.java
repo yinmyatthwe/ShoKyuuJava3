cd1be0c29ab9fea7cc854fb3940320c977cdd2b0
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-11 09:52:45 +0900

    9.2 キーが押されたときの働作を追加する

--- syokyuJava2-5/src/Car.java
+++ syokyuJava2-5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements KeyListener{
 	
 	public Car(int x,int y,int vx,int vy) {
 		super(x,y,vx,vy);
@@ -9,6 +11,31 @@ public class Car extends Vehicle {
 		frame.fillRect(x, y+20, 80, 20);
 		frame.fillOval(x+10, y+40, 20, 20);
 		frame.fillOval(x+50, y+40, 20, 20);
+	}
+	
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	public void keyPressed(KeyEvent e) {
+		//System.out.println("キーが押されました");
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+		
 	}
 	
 }
--- syokyuJava2-5/src/PlayerAnimationJava.java
+++ syokyuJava2-5/src/PlayerAnimationJava.java
@@ -1,7 +1,8 @@
 
 public class PlayerAnimationJava extends MyFrame {
 	public void run() {
-		Car player = new Car(100,200,3,0);
+		Car player = new Car(100,200,0,0);
+		addKeyListener(player);
 		while(true) {
 			clear();
 			player.draw(this);
